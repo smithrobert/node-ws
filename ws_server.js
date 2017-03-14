6b74e792184b1fd1a72130a414680322fcd7ec23
Marwan Khalili <khalili@Marwans-MBP.lan>
2017-03-14 11:36:43 +0100

    Fixed code style

--- ws_server.js
+++ ws_server.js
@@ -2,15 +2,12 @@ const WebSocket = require('ws');
 const https = require('https');
 const fs = require('fs');
 
-var files = [];
-for (let i = 0; i <= 100; i++) {
-  files[i] = fs.readFileSync('../assets/' + i + "k");
-}
-
 const options = {
   key: fs.readFileSync('../keys/privkey.pem'),
   cert: fs.readFileSync('../keys/fullchain.pem')
 };
+
+var file = fs.readFileSync('../assets/100k');
 
 var httpServer = new https.createServer(options).listen(443);
 var wss = new WebSocket.Server({
@@ -19,7 +16,7 @@ var wss = new WebSocket.Server({
 });
 
 wss.on('connection', function(ws) {
-  for (let i = 0; i < files.length; i++) {
-    ws.send(files[i]);
+  for (let i = 0; i < 100; i++) {
+    ws.send(file);
   }
 });
